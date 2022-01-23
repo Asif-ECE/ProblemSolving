8ab90213e2f7510a760cd86d44f598ea88d66bc8
Asif-ECE <nishatece13@gmail.com>
2022-01-23 14:26:20 +0600

    Task 5 and 6 completed

--- Python/pythonDjangoInterview63-1/main.py
+++ Python/pythonDjangoInterview63-1/main.py
@@ -2,12 +2,11 @@
 # You can use documentation:
 # https://docs.python.org/3/
 # https://docs.djangoproject.com/en/3.1/
-# *** TASK 1 ***
 
+
+# *** TASK 1 ***
 # Convert a given string to all uppercase if it contains at least
 # 2 uppercase characters in the first 4 characters
-
-
 
 def to_uppercase(string: str) -> str:
     count = 0
@@ -29,13 +28,10 @@ def to_uppercase(string: str) -> str:
 print(to_uppercase("PyTHon")) # returns PYTHON
 print(to_uppercase("PyThon")) # returns PyThon
 
-
 # *** TASK 1 END ***
 
 
-
 # *** TASK 2 ***
-
 # write code to print the following pattern
 
 def show_pattern(rc:int):
@@ -52,31 +48,38 @@ show_pattern(5) # prints out the pattern given below
 *****
 """
 
-
 # *** TASK 2 END ***
 
 
-
 # *** TASK 3 ***
-
 # Using argparse create a command that takes space separated numbers
 # and returns it is sum if the argument -s was provided and its multiplication
 # if the argument -m was provided
 
 import argparse
-from audioop import mul
 
-parse = argparse.ArgumentParser()
-parse.add_argument("ints", metavar='N', type=int, nargs='+')
-parse.add_argument("-s", "--sum", action="store_const", const=sum, default=1)
-args = parse.parse_args()
-print(args.sum(args.ints))
+
+class multiplyAction(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super().__init__(option_strings, dest, **kwargs)
+    def __call__(self, parser, namespace, values, option_string=None):
+        value = 0
+        print('%r %r %r' % (namespace, value, option_string))
+        setattr(namespace, self.dest, value)
+
+# parse = argparse.ArgumentParser()
+# parse.add_argument("ints", metavar='N', type=int, nargs='+')
+# parse.add_argument("-s", "--sum", dest='summation', action="store_const", const=sum)
+# parse.add_argument("-m", "--mul", dest='multiplication', action=multiplyAction)
+# args = parse.parse_args()
+# print(args.multiplication(args.ints))
+
 # *** TASK 3 END ***
 
 
-
 # *** TASK 4 ***
-
 # Sample from file:
 """
 write a method to get baby names from the files in the following director,
@@ -91,35 +94,29 @@ then print to 3 frequent names
 ...
 """
 
-"""
 directory = "baby_names/"
-
 
 # *** TASK 4 END ***
 
 
-
 # *** TASK 5 ***
-
 # Write a program in Python to check if a sequence is a Palindrome:
 
-
 def is_palindrome(word: str) -> bool:
-    pass
+    for i in range(len(word)//2):
+        if word[i] != word[len(word)-1-i]:
+            return False
+    return True
 
-is_palindrome("323") # returns True
-is_palindrome("tenet") # returns True
-is_palindrome("sequence") # returns False
-
+print(is_palindrome("323")) # returns True
+print(is_palindrome("tenet")) # returns True
+print(is_palindrome("sequence")) # returns False
 
 # *** TASK 5 END ***
 
 
-
 # *** TASK 6 ***
-
 # Remove duplicate dictionary elements (by name).
-
 
 student_data = {
     'id1': {
@@ -144,14 +141,24 @@ student_data = {
     },
 }
 
+def removeDup(stdDict:dict)->dict:
+    x = list(stdDict)
+    uniqueNames = []
+    for i in x:
+        if stdDict[i]['name'] in uniqueNames:
+            del stdDict[i]
+        else:
+            uniqueNames.append(stdDict[i]['name'])
+    print(stdDict)
+    return stdDict
+
+removeDup(student_data)
 
 # *** TASK 6 END ***
 
 
 # *** TASK 7 ***
-
 # Write Django settings for mysql database
-
 
 DATABASES = {
 
@@ -160,18 +167,13 @@ DATABASES = {
 # *** TASK 7 END ***
 
 
-
 # *** TASK 8 ***
-
 # Declare a composite index for fields first_name and last_name
 
+# from django.db import models
 
-from django.db import models
-
-
-class Customer(models.Model):
-	first_name = models.CharField(max_length=100)
-	last_name = models.CharField(max_length=100)
+# class Customer(models.Model):
+# 	first_name = models.CharField(max_length=100)
+# 	last_name = models.CharField(max_length=100)
 
 # *** TASK 8 END ***
-"""
